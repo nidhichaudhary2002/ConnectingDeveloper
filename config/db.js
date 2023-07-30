bde69b3b04e8e9abb210d1c0519f9c1a0b7f8f8c
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2023-07-31 01:09:14 +0530

    user registeration

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db,{useNewUrlParser:true});
+    await mongoose.connect(db, { useNewUrlParser: true });
     console.log('MongoDb Connected...');
   } catch (err) {
     console.error(err.message);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,61 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/Users');
 
-//@route    POSt api/users
+//@route    POST api/users
 //@desc     Register user
 //@access   Public
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //Check User existence
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      //Get users gravatoar
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+      user = new User({ name, email, avatar, password });
+
+      // Encrypt passpwrd
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
