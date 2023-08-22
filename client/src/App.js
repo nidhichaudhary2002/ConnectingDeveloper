14eedbe4a02fdae87078e761ea2cdcd0ee5445a2
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2023-08-22 19:47:39 +0530

    Register page frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar.js';
+import Landing from './components/layout/Landing.js';
+import Login from './components/auth/Login.js';
+import Register from './components/auth/Register.js';
 import './App.css';
 
 const App = () => (
   <Fragment>
-    <Navbar />
-    <Landing />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        {/* <section className='container'> */}
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+        {/* </section> */}
+      </Routes>
+    </BrowserRouter>
   </Fragment>
 );
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <section className='landing'>
+    <section className='landing '>
       <div className='dark-overlay'>
         <div className='landing-inner'>
           <h1 className='x-large'>Developer Connector</h1>
@@ -11,12 +12,12 @@ const Landing = () => {
             other developers
           </p>
           <div className='buttons'>
-            <a href='register.html' className='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' className='btn btn-light'>
+            </Link>
+            <Link to='/login' className='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
